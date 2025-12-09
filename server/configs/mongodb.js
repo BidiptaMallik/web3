4de2bb8340e4b0532c7db86b31f44925fbc181e0
Bidipta Mallik <bidiptamallik0@gmail.com>
2025-12-10 01:49:38 +0530

    jfja

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -2,10 +2,13 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('Database Connected');
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "CollegeAid",   
+    });
+
+    console.log("Database Connected to CollegeAid");
   } catch (err) {
-    console.error('MongoDB connection error:', err.message);
+    console.error("MongoDB connection error:", err.message);
   }
 };
 
